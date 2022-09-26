d301ab3e2d71d8343f3003e8383435e4fe9fd182
dlgeraghty <dvd.loge@gmail.com>
2022-09-26 22:23:10 +0200

    get marko up and running

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import marko
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p> hello world </p>"
+    with open('markdown_files/text1.md', 'r') as file:
+        data = file.read()
+    return marko.convert(data)
