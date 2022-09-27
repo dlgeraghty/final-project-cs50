4f7c29db73628ac06e270bea53855dd12a504baa
dlgeraghty <dvd.loge@gmail.com>
2022-09-27 22:18:49 +0200

    close for the day

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/blog")
+def blog():
+
+
 @app.route("/")
 def index():
     with open('markdown_files/text1.md', 'r') as file:
         data = file.read()
-    return render_template('base.html', data = marko.convert(data))
+    return render_template('index.html', data = marko.convert(data))
